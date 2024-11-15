b32a53afc17bd1c5cb874817ec6c311448bb1241
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-15 15:24:13 -0700

    feat: :tada: init
    - Create SvelteKit project (including Tailwind CSS) with `sv create`
    - Install `zod` for query param validation when implementing dynamic data
    - Import designs, assets, and starter text

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Frontend Mentor | Time tracking dashboard</title>
+</svelte:head>
+
+Report for Jeremy Robson Weekly Work 32hrs Previous - 36hrs Play 10hrs Previous - 8hrs Study 4hrs
+Previous - 7hrs Exercise 4hrs Previous - 5hrs Social 5hrs Previous - 10hrs Self Care 2hrs Previous -
+2hrs
